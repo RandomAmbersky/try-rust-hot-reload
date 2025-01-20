03361702e37cdeb25d3e993bed25242717fcce28
sir Random <RandomAmbersky@gmail.com>
2025-01-20 08:34:52 +0300

    some updates

--- app/src/main.js
+++ app/src/main.js
@@ -1,11 +1,11 @@
 console.log("Hello World from console");
 
-// import { init } from '../rustproject';
+import { init } from '../rustproject/pkg'
 
 // // Функция для запуска приложения после загрузки Wasm
-// async function run() {
-//     const wasm = await init(); // Инициализируем модуль Wasm
-//     console.log(wasm)
-// }
+async function run() {
+    const wasm = await init(); // Инициализируем модуль Wasm
+    console.log(wasm)
+}
 
-// run().catch(console.error);
+run().catch(console.error);
