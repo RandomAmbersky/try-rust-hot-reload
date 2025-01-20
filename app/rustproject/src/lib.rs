38dcabab42138f886c13a315ecd9649e93e98afe
sir Random <RandomAmbersky@gmail.com>
2025-01-20 13:17:35 +0300

    work with hot-reload by doker

--- app/rustproject/src/lib.rs
+++ app/rustproject/src/lib.rs
@@ -1,6 +1,17 @@
+extern crate console_error_panic_hook;
+use std::panic;
+
 use wasm_bindgen::prelude::*;
+
+/// Логирование сообщения в консоль браузера
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
 
 #[wasm_bindgen]
 pub fn init() {
-    println!("Hello, world2!");
+    panic::set_hook(Box::new(console_error_panic_hook::hook));
+    log("Hello, world from rust!");
 }
