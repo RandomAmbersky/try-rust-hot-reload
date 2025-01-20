afc5ebccbe45acc4f72c47c91fc9e92ddb99c7c0
sir Random <RandomAmbersky@gmail.com>
2025-01-20 20:03:10 +0300

    updates

--- app/rustproject/src/lib.rs
+++ app/rustproject/src/lib.rs
@@ -1,7 +1,26 @@
 extern crate console_error_panic_hook;
+use engine::IEngine;
 use std::panic;
-
 use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub struct EngineWebFacade {
+    e: Box<dyn IEngine>,
+}
+
+impl EngineWebFacade {
+    pub fn new() -> Self {
+        let engige = engine::new();
+        let e = Box::new(engige);
+        EngineWebFacade { e }
+    }
+}
+
+impl IEngine for EngineWebFacade {
+    fn add(&self, left: u64, right: u64) -> u64 {
+        return self.e.add(left, right);
+    }
+}
 
 /// Логирование сообщения в консоль браузера
 #[wasm_bindgen]
@@ -11,7 +30,8 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn init() {
+pub fn init() -> EngineWebFacade {
     panic::set_hook(Box::new(console_error_panic_hook::hook));
     log("Hello, world from rust!");
+    return EngineWebFacade {};
 }
